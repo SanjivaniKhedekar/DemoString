9d412be0f90da40b67e4ca168314efdc368c9b09
SanjivaniKhedekar@123 <sanjivanichavan48@gmail.com>
2022-03-16 11:21:44 +0530

    Third commit

---
+++ StringDemo.java
@@ -0,0 +1,16 @@
+package pkg3;
+
+public class StringDemo 
+{
+	public static void main(String[] args) 
+	 {
+		String s1="Hello String"; //used literals
+		char ch[]= {'J','a','v','a'};
+		String s2=new String(ch);
+		String s3=new String("Example");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		 }
+
+}
